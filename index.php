65fc165f46444959f96f83c7e29c4cf2648ea4cf
Maycon Luczynski <mayconsantlu@gmail.com>
2014-09-04 16:06:43 -0300

    Alterando o projeto

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-    require_once 'arquivos\includes\header.php';
+    require_once 'arquivos/includes/header.php';
 ?>
     <header>
     <?php
-        require_once 'arquivos\includes\menu.php';
+        require_once 'arquivos/includes/menu.php';
     ?>
 </header>
 <section>
     <?php
 
-    require_once 'arquivos\includes\Clientes.php';
-    require_once 'arquivos\includes\adicionar_clientes.php';
+    require_once 'arquivos/includes/Clientes.php';
+    require_once 'arquivos/includes/adicionar_clientes.php';
     //$clientes = new Clientes();
-    require_once 'arquivos\includes\dados.php';
+    require_once 'arquivos/includes/dados.php';
     ?>
 </section>
     <?php
-        require_once 'arquivos\includes\footer.php';
+        require_once 'arquivos/includes/footer.php';
     ?>
